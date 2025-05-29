0d68cf4931cbcedc1fed2acd626f5f6392dfb133
BojitaNoir <20213tn094@utez.edu.mx>
2025-05-28 18:11:01 -0600

    [ADD] Segundo commit [hubo cambios en un archivo]

--- src/main/java/utez/edu/mx/preexamen/pruebaxd.java
+++ src/main/java/utez/edu/mx/preexamen/pruebaxd.java
@@ -1,4 +1,16 @@
 package utez.edu.mx.preexamen;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user1")
+@CrossOrigin("*")
 
 public class pruebaxd {
+    @GetMapping("")
+    public String getMessage(){
+        return "Hola mundo!";
+    }
 }

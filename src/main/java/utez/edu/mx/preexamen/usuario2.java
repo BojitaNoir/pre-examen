1ca4ec448c3e16daeac4ead31897734cf05de334
BojitaNoir <20213tn094@utez.edu.mx>
2025-05-28 18:42:23 -0600

    [ADD] Tercer commit por user2 [modificacion en un archivo]

--- src/main/java/utez/edu/mx/preexamen/usuario2.java
+++ src/main/java/utez/edu/mx/preexamen/usuario2.java
@@ -1,4 +1,11 @@
 package utez.edu.mx.preexamen;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/user1")
+@CrossOrigin("*")
 public class usuario2 {
 }
